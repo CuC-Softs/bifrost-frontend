92d4a5dc75876a16dcf8805f7681354e6ee3af56
LOFyz <leonardo_farias2010@hotmail.com>
2021-03-20 16:07:15 -0300

    Ajuste da logo e da ul "HOT"

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -7,24 +7,27 @@ export const Header = styled.div`
   background-color: #edf4f4;
   justify-content: space-between;
   align-items: center;
-  padding: 0 2rem;
+  padding: 1.5rem;
 
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   #logo {
-    width: 12.5rem;
-    height: 6.3rem;
-    left: 2.1rem;
-    top: 3rem;
-    border-radius: 50%;
-
+    width: 18.6rem;
+    height: 9rem;
     background: #4db6ac;
+    border: none;
+    /* Me explica dps como usa imagens nesse framework, que n lembro */
+    background-image: url('../public/images/profile.svg');
+    background-repeat: no-repeat;
+    margin: 0;
+    padding: 0;
   }
 
   button {
     width: 6rem;
     height: 6rem;
     display: flex;
+    align-self: center;
 
     img,
     svg {
@@ -49,14 +52,14 @@ export const Body = styled.div`
     top: 21.5rem;
     margin-top: 4.8rem;
 
-    background: #E1EAE9;
+    background: #e1eae9;
     box-shadow: 0rem 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
     border-radius: 25px;
 
     justify-content: center;
     align-items: center;
 
-    color: #00625B;
+    color: #00625b;
     font-size: 1.8rem;
     font-family: 'Roboto Condensed', sans-serif;
     font-weight: 400;
@@ -99,16 +102,52 @@ export const Hot = styled.ul`
       width: 17rem;
       border-radius: 2.5rem;
       background: url() no-repeat;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+      div {
+        background: linear-gradient(rgba(0, 66, 61, 1), rgba(255, 255, 255, 0));
+        width: 100%;
+        height: 100%;
+        display: flex;
+        border-radius: 2.5rem;
+        justify-content: center;
+        align-items: flex-end;
+
+        label {
+          font-family: Roboto, sans-serif;
+          font-size: 2.3rem;
+          color: #fff;
+          width: 17rem;
+          height: 3rem;
+          text-align: center;
+          text-justify: center;
+        }
+      }
+    }
+    /* Isso é uma div */
+    #description {
+      background: rgba(235, 235, 235, 0.5);
+      display: flex;
+      align-items: center;
       justify-content: center;
-      align-items: flex-end;
+      flex-direction: column;
+
+      img,
+      svg {
+        width: 12.5rem;
+        height: 2.9rem;
+        justify-self: flex-start;
+        align-self: center;
+      }
 
       label {
+        color: #00867d;
         font-family: Roboto, sans-serif;
         font-size: 2.3rem;
-        color: #fff;
-        width: 17rem;
-        height: 3rem;
+        max-width: 11.8rem;
+        max-height: 15.2rem;
         text-align: center;
+        text-justify: center;
       }
     }
   }
