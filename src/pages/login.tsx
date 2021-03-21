d5e7172a5ae5862f5a5c5bb55bafeb135b51b500
Z1wolfmaster1 <suiciniv2016@outlook.com>
2021-03-21 17:38:36 -0300

    tela de login 90% pronta

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { ArrowBack } from '@material-ui/icons';
-import { Header } from '../styles/login';
+import { Header, Main } from '../styles/login';
 
 import Logo from '../components/Logo';
 
@@ -13,6 +13,41 @@ const pages: React.FC = () => (
       <ArrowBack />
       <Logo />
     </Header>
+    <Main>
+      <span id="connect">Sincronize-se com uma rede social</span>
+      <div id="carousel">
+        <span>
+          Ao sincronizar com uma rede social, você terá acesso à:
+          <br />
+          <br />
+          Seu próprio perfil no site, customizavel.
+        </span>
+        <span>
+          Ao sincronizar com uma rede social, você terá acesso à:
+          <br />
+          <br />
+          Compartilhamento de dados, como diários de bordo e as viagens que você
+          fez.
+        </span>
+        <span>
+          Ao sincronizar com uma rede social, você terá acesso à:
+          <br />
+          <br />
+          Comentar em posts, seguir, e ver perfis privados de ouras pessoas.
+        </span>
+        <span>
+          Ao sincronizar com uma rede social, você terá acesso à:
+          <br />
+          <br />
+          Acesso total ao site, com direito de poder criar sua própria aba de
+          viagens.
+        </span>
+        <button type="submit">
+          <span>Sincronize com o Google</span>{' '}
+        </button>
+        <div style={{ minWidth: '36.2rem' }} />
+      </div>
+    </Main>
   </>
 );
 
--- src/styles/login.ts
+++ src/styles/login.ts
@@ -7,10 +7,96 @@ export const Header = styled.header`
   align-items: center;
   padding-top: 4rem;
 
-  svg {
+  > svg {
     width: 3rem;
     height: 3rem;
+    margin-bottom: 6.9rem;
 
     color: #00867d;
+  }
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  margin-top: 6.7rem;
+
+  #connect {
+    display: flex;
+    width: 34.5rem;
+    height: 3.3rem;
+
+    background: #c7f3ef;
+    border-radius: 36rem;
+
+    align-items: center;
+    justify-content: center;
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.8rem;
+    line-height: 2.1rem;
+
+    color: #00867d;
+  }
+
+  #carousel {
+    display: flex;
+    max-width: 100%;
+    height: 24.2rem;
+    align-items: center;
+    margin-top: 4.4rem;
+    padding-left: 5.3rem;
+    overflow-x: scroll;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+
+    background: #edf4f4;
+    box-shadow: 0rem 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
+
+    span {
+      min-width: 31.6rem;
+
+      & + span {
+        margin-left: 4.6rem;
+      }
+
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 1.9rem;
+      line-height: 2.2rem;
+      text-align: center;
+
+      color: #4db6ac;
+    }
+
+    button {
+      display: flex;
+      flex-direction: column;
+      min-width: 31.6rem;
+      margin-left: 4.6rem;
+
+      span {
+        min-width: 25.6rem;
+        min-height: 2rem;
+
+        background: #00625b;
+        border-radius: 3.5rem;
+
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 1.3rem;
+        line-height: 1.5rem;
+
+        color: #a2e0da;
+      }
+    }
   }
 `;
